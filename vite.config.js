5694f886e4f1ab36d769f8857afccf5ff55ec371
MKCodeHub139 <ansarikafi757@gmail.com>
2026-04-26 11:04:45 +0530

    fixes vite cofig file

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
-     build: {
-        manifest: true,
-        outDir: 'public/build', // 🔥 IMPORTANT
-    },
-     base: '/',
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
+
+    base: '/',
 });
